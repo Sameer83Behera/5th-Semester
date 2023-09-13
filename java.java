034a9680c1c070bb6391cf4047793308cd419974
Sameer83Behera <beherassameer907@gmail.com>
2023-09-14 00:17:40 +0530

    Sameer

---
+++ java.java
@@ -0,0 +1,17 @@
+class A{
+    public static void main(String args[]){
+    int a,b,c;
+    a=10;
+    b=20;
+    c=5;
+    if (a>b && a>c){
+        System.out.println("a");
+    }
+    else if(b>c){
+        System.out.println("b");
+    }
+    else{
+        System.out.println("c");
+    }
+    }
+}
---
+++ python.py
@@ -0,0 +1,8 @@
+thisdict={
+    "brand":"ford",
+    "model":"Mustang",
+    "year":1964
+}
+print(thisdict)
+thisdict["color"]="red"
+print(thisdict)
